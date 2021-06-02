fc417c5daf32dfc8f3423a2dc234c2bcf3422c3b
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-03 01:10:50 +0300

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -46,6 +46,6 @@ async def inline(client: Client, query: InlineQuery):
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: Search timed out",
+                switch_pm_text="Error: Arama zamanlandı",
                 switch_pm_parameter="",
             )
