5f09bd21fdad666c32348736c9d9f0071912a6de
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-04 00:57:05 +0300

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from .config import API_HASH, API_ID, SESSION_NAME
-from .callsmusic import queues
+from config import API_HASH, API_ID, SESSION_NAME
+from callsmusic import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
