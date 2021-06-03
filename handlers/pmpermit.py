3a7abbfbace1e0bc0cfea8c7839c591764d60a3a
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-03 22:03:09 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -1,10 +1,68 @@
-from callsmusic.callsmusic import client as USER
+from pyrogram import Client
+import asyncio
+from DaisyXMusic.config import SUDO_USERS, PMPERMIT
 from pyrogram import filters
-from pyrogram.types import Chat, Message, User
+from pyrogram.types import Message
+from DaisyXMusic.services.callsmusic.callsmusic import client as USER
 
+PMSET =True
+pchats = []
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Merhaba, Bu bir müzik asistanı hizmetidir.\n\n ❗️ kurallar:\n   - Sohbete izin yok\n   - İstenmeyen postaya izin verilmez \n\n 🚨 **USERBOT GRUBUNUZA KATILAMAZSA GRUP DAVETI BAĞLANTISI VEYA KULLANICI ADI GÖNDER.**\n\n ⚠️ DİKKAT: Burada bir mesaj gönderiyorsanız Yöneticinin iletinizi göreceği anlamına gelir ve sohbete katılın\n    - Bu kullanıcıyı gizli gruplara eklemeyin. 🤖 VERSİON 3.2.8\n   - Özel bilgileri burada paylaşmayın\n\n")
-  return                        
- 
+    if PMPERMIT == "ENABLE":
+        if PMSET:
+            chat_id = message.chat.id
+            if chat_id in pchats:
+                return
+            await USER.send_message(
+                message.chat.id,
+                "**Voice Müzik Asistanına Hoş Geldiniz**\n\n**🛑 Rules:**\n1. Burada spam yapmayın. \n2. Burada Sohbet Etmek Yok.\n\n**⚠️ Disclamer:** Burada bir mesaj gönderiyorsanız, yönetici mesajınızı görecek ve sohbete katılacaktır\n    - Bu kullanıcıyı gizli gruplara ekleme.😉\n   - Özel bilgileri burada paylaşmayın 👨‍💻\n\n",
+            ) 
+            return
+
+    
+
+@Client.on_message(filters.command(["/pmpermit"]))
+async def bye(client: Client, message: Message):
+    if message.from_user.id in SUDO_USERS:
+        global PMSET
+        text = message.text.split(" ", 1)
+        queryy = text[1]
+        if queryy == "on":
+            PMSET = True
+            await message.reply_text("Pmpermit turned on")
+            return
+        if queryy == "off":
+            PMSET = None
+            await message.reply_text("Pmpermit turned off")
+            return
+
+@USER.on_message(filters.text & filters.private & filters.me)        
+async def autopmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM due to outgoing messages")
+        return
+    message.continue_propagation()    
+    
+@USER.on_message(filters.command("a", [".", ""]) & filters.me & filters.private)
+async def pmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if not chat_id in pchats:
+        pchats.append(chat_id)
+        await message.reply_text("Approoved to PM")
+        return
+    message.continue_propagation()    
+    
+
+@USER.on_message(filters.command("da", [".", ""]) & filters.me & filters.private)
+async def rmpmPermiat(client: USER, message: Message):
+    chat_id = message.chat.id
+    if chat_id in pchats:
+        pchats.remove(chat_id)
+        await message.reply_text("Dispprooved to PM")
+        return
+    message.continue_propagation() 
+
