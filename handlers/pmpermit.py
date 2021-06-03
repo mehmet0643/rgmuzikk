1ff345346f5568769d5703ff25c60b4da191f362
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-04 00:57:43 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -1,9 +1,9 @@
 from pyrogram import Client
 import asyncio
-from .config import SUDO_USERS, PMPERMIT
+from config import SUDO_USERS, PMPERMIT
 from pyrogram import filters
 from pyrogram.types import Message
-from .callsmusic.callsmusic import client as USER
+from callsmusic import client as USER
 
 PMSET =True
 pchats = []
