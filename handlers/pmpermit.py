7ec0eac2e512b3cce4b204a9347585f81ec71390
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-04 02:15:51 +0300

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -7,49 +7,4 @@ from pyrogram.types import Chat, Message, User
 async def pmPermit(client: USER, message: Message):
   await USER.send_message(message.chat.id,"Merhaba, Bu bir müzik asistanı hizmetidir.\n\n ❗️ kurallar:\n   - Sohbete izin yok\n   - İstenmeyen postaya izin verilmez \n\n 🚨 **USERBOT GRUBUNUZA KATILAMAZSA GRUP DAVETI BAĞLANTISI VEYA KULLANICI ADI GÖNDER.**\n\n ⚠️ DİKKAT: Burada bir mesaj gönderiyorsanız Yöneticinin iletinizi göreceği anlamına gelir ve sohbete katılın\n    - Bu kullanıcıyı gizli gruplara eklemeyin.\n   - Özel bilgileri burada paylaşmayın\n\n")
   return                        
-           
-    
-
-@Client.on_message(filters.command(["/pmpermit"]))
-async def bye(client: Client, message: Message):
-    if message.from_user.id in SUDO_USERS:
-        global PMSET
-        text = message.text.split(" ", 1)
-        queryy = text[1]
-        if queryy == "on":
-            PMSET = True
-            await message.reply_text("Pmpermit turned on")
-            return
-        if queryy == "off":
-            PMSET = None
-            await message.reply_text("Pmpermit turned off")
-            return
-
-@USER.on_message(filters.text & filters.private & filters.me)        
-async def autopmPermiat(client: USER, message: Message):
-    chat_id = message.chat.id
-    if not chat_id in pchats:
-        pchats.append(chat_id)
-        await message.reply_text("Approoved to PM due to outgoing messages")
-        return
-    message.continue_propagation()    
-    
-@USER.on_message(filters.command("izin", [".", ""]) & filters.me & filters.private)
-async def pmPermiat(client: USER, message: Message):
-    chat_id = message.chat.id
-    if not chat_id in pchats:
-        pchats.append(chat_id)
-        await message.reply_text("Approoved to PM")
-        return
-    message.continue_propagation()    
-    
-
-@USER.on_message(filters.command("sus", [".", ""]) & filters.me & filters.private)
-async def rmpmPermiat(client: USER, message: Message):
-    chat_id = message.chat.id
-    if chat_id in pchats:
-        pchats.remove(chat_id)
-        await message.reply_text("Dispprooved to PM")
-        return
-    message.continue_propagation() 
 
