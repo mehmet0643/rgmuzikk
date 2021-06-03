1986868a492e63738b794ef874fe5ce38e3650b3
Mehmetbaba06 <83150866+Mehmetbaba06@users.noreply.github.com>
2021-06-03 11:26:48 +0300

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -78,34 +78,75 @@ async def a(client, message: Message):
         print(e)
 
 
-# saavn dan indirmek için. 
-@Client.on_message(filters.command("saavn") & ~filters.edited)
-async def jsbul(_, message):
+# YouTube çalan müzik i vdeo olarak indirmek için.
+ 
+@Client.on_message(filters.command(["vindir", "video"]))
+async def ytmusic(client,message: Message):
     global is_downloading
-    if len(message.command) < 2:
-        await message.reply_text("/saavn requires an argument.")
-        return
     if is_downloading:
-        await message.reply_text("Başka bir indirme devam ediyor, bir süre sonra yeniden deneyin.")
+        await message.reply_text("Another download is in progress, try again after sometime.")
         return
-    is_downloading = True
-    text = message.text.split(None, 1)[1]
-    query = text.replace(" ", "%20")
-    m = await message.reply_text("🌍 Aranıyor... Efendim!")
+
+    urlissed = get_text(message)
+
+    pablo =  await client.send_message(
+            message.chat.id,
+            f"`📩 Alıyorum {urlissed} Youtube Sunucularından. Lütfen bekleyin.`")
+    if not urlissed:
+        await pablo.edit("Geçersiz Komut Sözdizimi, Daha fazla şey öğrenmek için lütfen Yardım menüsünü kontrol edin!")
+        return
+    
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+            "format": "best",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {"key": "FFmpegVideoConvertor", "tercih edilen biçim": "mp4"}
+            ],
+            "outtmpl": "%(id)s.mp4",
+            "logtostderr": False,
+            "quiet": True,
+        }
     try:
-        songs = await arq.saavn(query)
-        sname = songs[0].song
-        slink = songs[0].media_url
-        ssingers = songs[0].singers
-        await m.edit("Downloading")
-        song = await download_song(slink)
-        await m.edit("Uploading")
-        await message.reply_audio(audio=song, title=sname,
-                                  performer=ssingers)
-        os.remove(song)
-        await m.delete()
+        is_downloading = True
+        with youtube_dl.YoutubeDL(opts) as ytdl:
+            infoo = ytdl.extract_info(url, False)
+            duration = round(infoo["duration"] / 60)
+
+            if duration > 8:
+                await pablo.edit(
+                    f"❌ 8 dakikadan uzun videolar(s) izin verilmezse, sağlanan video{duration} dakika(s)"
+                )
+                is_downloading = False
+                return
+            ytdl_data = ytdl.extract_info(url, download=True)
+            
+    
     except Exception as e:
+        #await pablo.edit(event, f"**Failed To Download** \n**Error :** `{str(e)}`")
         is_downloading = False
-        await m.edit(str(e))
         return
+    
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"**Video Name ➠** `{thum}` \n**Requested For :** `{urlissed}` \n**Channel :** `{thums}` \n**Link :** `{mo}`"
+    await client.send_video(message.chat.id, video = open(file_stark, "rb"), duration = int(ytdl_data["duration"]), file_name = str(ytdl_data["title"]), thumb = sedlyf, caption = capy, supports_streaming = True , progress=progress, progress_args=(pablo, c_time, f'`Uploading {urlissed} Song From YouTube Music!`', file_stark))
+    await pablo.delete()
     is_downloading = False
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
